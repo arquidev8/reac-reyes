e0d8091183068bd255ab8d4d4191302846713ca6
arquidev8 <98498528+arquidev8@users.noreply.github.com>
2022-11-24 15:22:57 -0400

    6 files modified

--- src/App.js
+++ src/App.js
@@ -1,41 +1,81 @@
 import React from 'react';
 import './style.css';
 import Hijo from './Hijo';
+import { useState } from 'react';
 
 export default function App() {
-  const ReyGodo1 = {
-    nombre: 'Ervigio',
-    vacas: 3,
-    imagen: 'https://www.html6.es/img/rey_ervigio.png',
-  };
+  const objects = [
+    {
+      nombre: 'Ervigio',
+      vacas: 3,
+      imagen: 'https://www.html6.es/img/rey_ervigio.png',
+    },
 
-  const ReyGodo2 = {
-    nombre: 'Atanagildo',
-    vacas: 6,
-    imagen: 'https://www.html6.es/img/rey_atanagildo.png',
-  };
+    {
+      nombre: 'Atanagildo',
+      vacas: 6,
+      imagen: 'https://www.html6.es/img/rey_atanagildo.png',
+    },
 
-  const ReyGodo3 = {
-    nombre: 'AtaÃºlfo',
-    vacas: 11,
-    imagen: 'https://www.html6.es/img/rey_ataulfo.png',
-  };
+    {
+      nombre: 'AtaÃºlfo',
+      vacas: 11,
+      imagen: 'https://www.html6.es/img/rey_ataulfo.png',
+    },
 
-  const ReyGodo4 = {
-    nombre: 'Leogivildo',
-    vacas: 2,
-    imagen: 'https://www.html6.es/img/rey_leogivildo.png',
+    {
+      nombre: 'Leogivildo',
+      vacas: 2,
+      imagen: 'https://www.html6.es/img/rey_leogivildo.png',
+    },
+  ];
+
+  const [mensaje, setMensaje] = useState('vota por tu rey favorito');
+  const sumar = (nombre, valoracion) => {
+    setMensaje(
+      <div>
+        Has votado por el Rey: {nombre} con una valoracion de: {valoracion}
+      </div>
+    );
   };
 
   return (
-    <div>
-      <Hijo
-        datos={{
-          nombre: 'Ervigio',
-          vacas: 11,
-          imagen: 'https://www.html6.es/img/rey_ervigio.png',
-        }}
-      />
-    </div>
+    <>
+      <h2>{mensaje}</h2>
+      <div className="container-fluid">
+        <Hijo
+          onAction={sumar}
+          datos={{
+            nombre: 'Ervigio',
+            vacas: 11,
+            imagen: 'https://www.html6.es/img/rey_ervigio.png',
+          }}
+        />
+        <Hijo
+          onAction={sumar}
+          datos={{
+            nombre: 'Atanagildo',
+            vacas: 6,
+            imagen: 'https://www.html6.es/img/rey_atanagildo.png',
+          }}
+        />
+        <Hijo
+          onAction={sumar}
+          datos={{
+            nombre: 'ataulfo',
+            vacas: 11,
+            imagen: 'https://www.html6.es/img/rey_ataulfo.png',
+          }}
+        />
+        <Hijo
+          onAction={sumar}
+          datos={{
+            nombre: 'leogivildo',
+            vacas: 2,
+            imagen: 'https://www.html6.es/img/rey_leogivildo.png',
+          }}
+        />
+      </div>
+    </>
   );
 }
--- src/Hijo.js
+++ src/Hijo.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
-const Hijo = ({ datos }) => {
+const Hijo = ({ datos, onAction }) => {
   const { nombre, vacas, imagen } = datos;
+  const [valoracion, setValoracion] = useState(0);
+
+  const votar = () => {
+    setValoracion(valoracion + 1);
+  };
+
+  useEffect(() => {
+    onAction(nombre, valoracion);
+  }, [valoracion]);
 
   return (
     <div className="cadaRey">
-      <div><h1> {nombre} </h1></div>
-      <div><h2>come {vacas} vacas al dia</h2></div>
-      <img src={imagen} alt="ervigio"></img>
+      <div>
+        <h1> {nombre} </h1>
+      </div>
+      <div className="menu">
+        <h2>come {vacas} vacas al dia</h2>
+      </div>
+      <img className="img-fluid" src={imagen} alt="ervigio"></img>
+      <div className="cajaVotacion">
+        <button onClick={() => votar()}>Votame</button>
+        <div className="votacion">{valoracion}</div>
+      </div>
     </div>
   );
 };
